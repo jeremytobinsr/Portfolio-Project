b0469803b0bc8147bb705497c27441917ec11dbe
Jeremy Tobin <jeremy.tobin@talent.cancode.org>
2024-02-02 21:24:51 -0500

    fixed .gitignore file and changed Javascript

--- index.js
+++ index.js
@@ -18,13 +18,8 @@ async function makeApiCalls(){
   try {
     const response1 = await axios.get('');
 
-    console.log(response.data);
-
     const response2 = await axios.get('');
 
-    console.log(response2.data);
-
     } catch (error) {
-      console.error(error);
+    
     }}
-}
